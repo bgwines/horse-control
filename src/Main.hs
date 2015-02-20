ec1951fe4999dd60b0e742fd8487bce456eb3842
Brett Wines <brettwines@gmail.com>
2015-02-19 19:06:55 -0800

    Basic command-line argument parsing

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,34 @@
+import System.Environment
+import System.Exit
+
+import qualified Horse.Commands as Commands
+
+helpText :: String
+helpText = "<insert help text here>"
+
+version :: String
+version = "v0.1.0.0"
+
+parseArgs :: [String] -> IO String
+parseArgs [] = parseArgs ["-h"]
+parseArgs args =
+    case head args of
+        "add"           -> (Commands.add         $ tail args ) >> exit
+        "rm"            -> (Commands.rm          $ tail args ) >> exit
+        "checkout"      -> (Commands.checkout    $ tail args ) >> exit
+        "commit"        -> (Commands.commit      $ tail args ) >> exit
+        "diff"          -> (Commands.diff        $ tail args ) >> exit
+        "init"          -> (Commands.init        $ tail args ) >> exit
+        "log"           -> (Commands.log         $ tail args ) >> exit
+        "status"        -> (Commands.status      $ tail args ) >> exit
+        "show"          -> (Commands.show        $ tail args ) >> exit
+        "-v"            -> (putStrLn version                 ) >> exit
+        "-h"            -> (putStrLn helpText                ) >> exit
+        _               -> ((putStrLn $ "Unrecognized command: "
+                                         ++ (head args))     ) >> die
+    where
+        exit = exitWith ExitSuccess
+        die  = exitWith (ExitFailure 1)
 
 main :: IO ()
-main = do
-    putStrLn $ "Hello, World!"
+main = getArgs >>= parseArgs >>= putStr
